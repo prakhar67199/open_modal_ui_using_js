702dd532408be5004063b4134cf04ed84157a46e
prakhar chandra gupta <prakharguptahr10@gmail.com>
2023-04-27 01:35:55 +0530

    madal__UI

---
+++ index.js
@@ -0,0 +1,18 @@
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+
+// Modal  open function
+const openModal = ()=>
+{
+    console.log("Modal is open");
+    modal.classList.add("active");
+    overlay.classList.add("overlayactive");
+};
+
+// modal close function
+const closeModal = () =>
+{
+    console.log("Modal is closed");
+    modal.classList.remove("active");
+    overlay.classList.remove("overlayactive");
+}
